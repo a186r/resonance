7f59a7cd62562f4be5514b697d8eb71bcfd3033b
XatMassacrE <xxat.massacre@gmail.com>
2019-05-30 03:06:17 +0800

    Add fromWei format bignumber

--- web/src/store.js
+++ web/src/store.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import Web3 from 'web3'
 import { formatEth, ethToWei } from './utils/ethUtils'
 
+const web3 = new Web3()
 
 Vue.use(Vuex)
 
@@ -19,15 +21,17 @@ export default new Vuex.Store({
       remainingToken: 3000,
       remainingETH: 23,
       totalTokenAmount: 5000000,
+      totalETHAmount: 300,
+      eachAddressLimit: 20000
     },
     homeData: {
       currentStepTokenAmount: 10000000,
       currentStepRaisedETH: 100.1234,
-      stepIndex: 1
+      stepIndex: 0
     },
-    rewardList: {
-
-    }
+    rewardList: [
+      [13, ['0x7525c82e0cf1832e79ff3aff259c5fe853cf95f4'], [1.3]]
+    ]
   },
   mutations: {
     GET_FUNDER_INFO: (state, data) => {
@@ -38,19 +42,18 @@ export default new Vuex.Store({
     },
     GET_CURRENT_STEP_FUNDS_INFO: (state, data) => {
       Object.assign(state.homeData, data)
-      // data.this.dispatch('queryRewardList', state.homeData.stepIndex)
     },
     GET_ADDRESS_IS_BUILDER: (state, data) => {
       state.isBuilder = data
     },
     GET_REWARD_LIST: (state, data) => {
-      console.log('reward ', data)
-      state.rewardList = data
+      console.log('reward ', data.index, data)
+      state.rewardList[data.index] = data
     }
   },
   actions: {
     async getFunderInfo({ commit }, contract) {
-      contract.methods.getFunderInfo(this.state.account).call({}, (err, result) => {
+      contract.methods.getFunderInfo().call({}, (err, result) => {
         console.log(err, 'get funder info', result, contract.address)
       })
       commit('GET_FUNDER_INFO', [
@@ -65,36 +68,60 @@ export default new Vuex.Store({
       ])
     },
     async getBuildingPeriodInfo({ commit }, contract) {
-      contract.methods.getBuildingPerioInfo().call({}, (err, result) => {
-        console.log(err, 'getBuildingPerioInfo', result, contract.address)
-        commit('GET_OFFER_INFO', {})
-      })
+      contract.methods.getBuildingPerioInfo().call()
+        .then(result => {
+          console.log('get building info', result)
+          const data = {}
+          // data.bpCountdown = result[0].toNumber()
+          data.remainingToken = web3.utils.fromWei(result[1].toString())
+          data.eachAddressLimit = web3.utils.fromWei(result[2].toString())
+          data.totalTokenAmount = web3.utils.fromWei(result[3].toString())
+          commit('GET_OFFER_INFO', data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
     },
     async getFundingPeriodInfo({ commit }, contract) {
-      contract.methods.getFundingPeriodInfo().call({}, (err, result) => {
-        console.log(err, 'getFundingPeriodInfo', result, contract.address)
-        commit('GET_OFFER_INFO', {})
-      })
+      contract.methods.getFundingPeriodInfo().call()
+        .then(result => {
+          console.log('get funding info', result)
+          const data = {}
+          data.fpCountdown = result[0].toNumber()
+          data.remainingETH = web3.utils.fromWei(result[1].toString())
+          data.totalETHAmount = web3.utils.fromWei(result[2].toString())
+          commit('GET_OFFER_INFO', data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
     },
     async getCurrentStepFundsInfo({ commit }, contract) {
       const self = this
-      contract.methods.getCurrentStepFundsInfo().call({}, (err, result) => {
-        console.log(err, 'getCurrentStepFundsInfo', result, contract.address)
-        result.currentStepRaisedETH = 200.13
-        // result.this = self
-        commit('GET_CURRENT_STEP_FUNDS_INFO', result)
-      })
+      contract.methods.getCurrentStepFundsInfo().call()
+        .then(result => {
+          const data = {
+            stepIndex: result[0].toNumber(),
+            currentStepTokenAmount: web3.utils.fromWei(result[1].toString()),
+            currentStepRaisedETH: web3.utils.fromWei(result[2].toString()),
+          }
+          commit('GET_CURRENT_STEP_FUNDS_INFO', data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
     },
     async depositETH({ commit }, amount) {
-      console.log(window.contract, amount)
       const self = this
-      web3.eth.sendTransaction({
-        from: this.state.account,
+      const data = {
+        from: self.state.account,
         to: window.contract.address,
-        value: ethToWei(amount)
-      })
-      .then(function(receipt){
-        console.log(receipt)
+        value: web3.utils.toWei(amount),
+        gas: '0x52080'
+      }
+      window.web3.eth.sendTransaction(data)
+      .then(res => {
+        console.log(res)
       }).catch(err => {
         self._vm.$alert('Metamask 提交失败', '提示', {
           confirmButtonText: '确定',
@@ -103,14 +130,25 @@ export default new Vuex.Store({
     },
     async depositCAD({ commit }, amount) {
       // solidity function name: jointlyBuild
-      console.log(amount)
       const self = this
-      window.contract.methods.jointlyBuild(ethToWei(amount)).send({
-        from: this.state.account,
+      window.contract.methods.jointlyBuild(web3.utils.toWei(amount)).send({
+        from: self.state.account,
       }).then(res => {
         console.log(res)
       }).catch(err => {
         self._vm.$alert('Metamask 提交失败', '提示', {
+          confirmButtonText: '确定',
+        })
+      })
+    },
+    async approve({ commit }, amount) {
+      const self = this
+      window.tokenContract.methods.approve(window.contract.address, web3.utils.toWei(amount)).send({
+        from: self.state.account
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        self._vm.$alert('授权合约失败', '提示', {
           confirmButtonText: '确定',
         })
       })
@@ -165,25 +203,25 @@ export default new Vuex.Store({
       contract.methods.getFissionRewardInfo(stepIndex).call({
         from: this.state.account,
       }).then(res => {
-        const data = {
-
-        }
-        console.log('fission reward', res)
+        res.index = 0
         commit('GET_REWARD_LIST', res)
       })
       contract.methods.getFOMORewardIofo(stepIndex).call({
         from: this.state.account,
       }).then(res => {
+        res.index = 1
         commit('GET_REWARD_LIST', res)
       })
       contract.methods.getLuckyRewardInfo(stepIndex).call({
         from: this.state.account,
       }).then(res => {
+        res.index = 2
         commit('GET_REWARD_LIST', res)
       })
       contract.methods.getFaithRewardInfo().call({
         from: this.state.account,
       }).then(res => {
+        res.index = 3
         commit('GET_REWARD_LIST', res)
       })
     },
